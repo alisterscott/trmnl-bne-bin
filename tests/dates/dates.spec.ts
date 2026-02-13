8b702ebb6bc0f4439127b03e7b23431765463ffb
Alister Scott <alister.scott@gmail.com>
2026-02-13 17:43:16 +1000

    add tests for this week and next week

--- tests/dates/dates.spec.ts
+++ tests/dates/dates.spec.ts
@@ -95,3 +95,61 @@ test("can see a message when bin collection day is tomorrow but next week", asyn
     fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
   }
 });
+
+test("can see a message when bin collection day beyond tomorrow this week (Wednesday)", async ({
+  page,
+}) => {
+  const sourceFile = path.join(__dirname, "beyondtomorrowthisweek.trmnlp.yml");
+  const destFile = path.join(__dirname, "..", "..", ".trmnlp.yml");
+
+  if (!fs.existsSync(sourceFile)) {
+    throw new Error(`Source file not found: ${sourceFile}`);
+  }
+
+  if (!fs.existsSync(destFile)) {
+    throw new Error(`Destination file not found: ${destFile}`);
+  }
+
+  const originalContent = fs.readFileSync(destFile, "utf-8"); // Save original content to restore later
+
+  fs.copyFileSync(sourceFile, destFile);
+
+  try {
+    await page.goto("/full");
+    const trmnlFrame = page.frameLocator("iframe");
+    await expect(trmnlFrame.locator("div.title.later")).toHaveText(
+      "Your next bin collection is this Wednesday",
+    );
+  } finally {
+    fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
+  }
+});
+
+test("can see a message when bin collection day beyond tomorrow next week (Monday)", async ({
+  page,
+}) => {
+  const sourceFile = path.join(__dirname, "beyondtomorrownextweek.trmnlp.yml");
+  const destFile = path.join(__dirname, "..", "..", ".trmnlp.yml");
+
+  if (!fs.existsSync(sourceFile)) {
+    throw new Error(`Source file not found: ${sourceFile}`);
+  }
+
+  if (!fs.existsSync(destFile)) {
+    throw new Error(`Destination file not found: ${destFile}`);
+  }
+
+  const originalContent = fs.readFileSync(destFile, "utf-8"); // Save original content to restore later
+
+  fs.copyFileSync(sourceFile, destFile);
+
+  try {
+    await page.goto("/full");
+    const trmnlFrame = page.frameLocator("iframe");
+    await expect(trmnlFrame.locator("div.title.later")).toHaveText(
+      "Your next bin collection is next Monday",
+    );
+  } finally {
+    fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
+  }
+});
